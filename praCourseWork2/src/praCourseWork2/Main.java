4b96e4ba575002f5ab2c0347fbcb4b4e99f0f0f9
Tahmidul Islam <k1462015@kcl.ac.uk>
2015-02-10 22:05:01 +0000

    Requirement 1 - Made a JFrame for the program. Set title to PRA Coursework - TeamName.

---
+++ praCourseWork2/src/praCourseWork2/Main.java
@@ -0,0 +1,12 @@
+package praCourseWork2;
+
+import javax.swing.JFrame;
+
+public class Main {
+
+	public static void main(String[] args) {
+		JFrame frame = new StudentFrame();
+
+	}
+
+}
---
+++ praCourseWork2/src/praCourseWork2/StudentFrame.java
@@ -0,0 +1,14 @@
+package praCourseWork2;
+
+import javax.swing.JFrame;
+
+public class StudentFrame extends JFrame{
+	
+	public StudentFrame(){
+		//Initialises frame and sets title to team name
+		super("PRA Coursework - TMH");
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setVisible(true);
+	}
+
+}
