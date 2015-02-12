66779ccae57ae317e76fcd002d17705556c83f9f
Tahmidul Islam <k1462015@kcl.ac.uk>
2015-02-12 17:41:41 +0000

    Set Frame to visible and set the size of it accordingly. Also started to create a method called makePopUp which will create the actual Pop Up, I then called it within the constructor. I also added a pack() method in order to make pop up neater after it is created.

--- praCourseWork2/src/praCourseWork2/DisplayPopUpFrame.java
+++ praCourseWork2/src/praCourseWork2/DisplayPopUpFrame.java
@@ -15,20 +15,38 @@ public class DisplayPopUpFrame extends JFrame {
 	private JLabel tutor;
 	private JPanel main;
 	private JPanel bottom;
-	
-	public DisplayPopUpFrame(Student student){
+
+	public DisplayPopUpFrame(Student student) {
 		super("Student Information - Display");
-		//Initialises all required fields with constructor arguments
+		// Initialises all required fields with constructor arguments
 		this.name = new JLabel(student.name);
 		this.emailAddress = new JLabel(student.email);
-		this.studentNumber = new JLabel("Student No. :   "+student.studentNumber);
-		this.tutor = new JLabel("Tutor:     "+student.tutor);
-		
-		//Initialises requried JPanels
-				main = new JPanel();
-				bottom = new JPanel();
+		this.studentNumber = new JLabel("Student No. :   "
+				+ student.studentNumber);
+		this.tutor = new JLabel("Tutor:     " + student.tutor);
 
-	
+		// Initialises requried JPanels
+		main = new JPanel();
+		bottom = new JPanel();
+
+		// Required JFrame
+		setVisible(true);
+		setSize(700, 500);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		getRootPane().setBorder(
+				BorderFactory.createMatteBorder(4, 4, 4, 4, Color.RED));
+		setBackground(Color.white);
+
+		// Calls Popup method to make required JPanel
+		makePopUp();
+
+		// Packs all panels to make neater
+		pack();
+
+	}
+
+	public void makePopUp() {
+
 	}
 
 }
