54ebf10e6243486c715fa7489e1ae36acc95313a
Tahmidul Islam <k1462015@kcl.ac.uk>
2015-02-12 17:44:21 +0000

    Filled in the makePopUp method. Set the Frame to a BoxLayout. Changed font size of all JLabels. I then aligned all JLabels in their corresponding allignments. Also set email address to italics.

--- praCourseWork2/src/praCourseWork2/DisplayPopUpFrame.java
+++ praCourseWork2/src/praCourseWork2/DisplayPopUpFrame.java
@@ -1,11 +1,15 @@
 package praCourseWork2;
 
+import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Font;
 
 import javax.swing.BorderFactory;
+import javax.swing.BoxLayout;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.SwingConstants;
 
 public class DisplayPopUpFrame extends JFrame {
 	private static final long serialVersionUID = 1L;
@@ -46,6 +50,38 @@ public class DisplayPopUpFrame extends JFrame {
 	}
 
 	public void makePopUp() {
+		//Sets JPanel to BoxLayout
+		//setLayout(new BoxLayout(this, BoxLayout.PAGE_AXIS));
+		main.setLayout(new BorderLayout());
+		
+		//Sets font size
+		name.setFont (this.name.getFont ().deriveFont (64.0f));
+		//this.emailAddress.setFont (this.emailAddress.getFont ().deriveFont (40.0f));
+		studentNumber.setFont (this.studentNumber.getFont ().deriveFont (32.0f));
+		tutor.setFont (this.tutor.getFont ().deriveFont (32.0f));
+
+		//Aligns all JLabels
+		name.setHorizontalAlignment(SwingConstants.CENTER);
+		emailAddress.setHorizontalAlignment(SwingConstants.CENTER);
+		studentNumber.setHorizontalAlignment(SwingConstants.LEFT);
+		tutor.setHorizontalAlignment(SwingConstants.LEFT);
+
+		//Sets email address to italics
+		emailAddress.setFont(new Font("Arial",Font.ITALIC,40));
+		
+		//Adds sNumber and tutor email to bottom JPanel
+		bottom.setLayout(new BoxLayout(bottom,BoxLayout.PAGE_AXIS));
+		bottom.add(this.studentNumber);
+		bottom.add(this.tutor);
+		
+		//Adds all labels to the main Panel
+		main.add(this.name, BorderLayout.NORTH);
+		main.add(this.emailAddress, BorderLayout.CENTER);
+		main.add(bottom, BorderLayout.SOUTH);
+		
+		//Adds main panel to Main JFrame
+		add(main);
+
 
 	}
 
