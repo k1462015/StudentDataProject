2cee8982ae9e9b33f244c987363f9ea2b83e392a
Tahmidul Islam <k1462015@kcl.ac.uk>
2015-02-13 01:00:10 +0000

    Just some visual changes.

--- praCourseWork2/src/praCourseWork2/StudentFrame.java
+++ praCourseWork2/src/praCourseWork2/StudentFrame.java
@@ -1,6 +1,7 @@
 package praCourseWork2;
 
 import java.awt.BorderLayout;
+import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyAdapter;
@@ -25,85 +26,92 @@ import javax.swing.JTextField;
 import studentdata.Connector;
 import studentdata.DataTable;
 
+public class StudentFrame extends JFrame {
 
-public class StudentFrame extends JFrame{
-	
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-	public StudentFrame(){
-		//Initialises frame and sets title to team name
+
+	public StudentFrame() {
+		// Initialises frame and sets title to team name
 		super("PRA Coursework - TMH");
-		setSize(400,400);//MR:added size
+		setSize(700, 500);// MR:added size
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
-		setLocationRelativeTo(null);//MR:added location
-		
-		JPanel panel = new JPanel();//panel to contain other components
+		setLocationRelativeTo(null);// MR:added location
+
+		JPanel panel = new JPanel();// panel to contain other components
 
 		JMenuBar menu = new JMenuBar();
 		JMenu file = new JMenu("File");
 		menu.add(file);
 		JMenuItem load = new JMenuItem("Load anonymous marking codes");
 		load.addActionListener(new ActionListener() {
-			
-			public void actionPerformed(ActionEvent e){
-				
+
+			public void actionPerformed(ActionEvent e) {
+
 				JFileChooser choosy = new JFileChooser();
 				File f = new File("C://Users//Saif//workspace");
-				
+
 				choosy.setCurrentDirectory(f);
+
+				choosy.showOpenDialog(null);
+
+
 				choosy.showOpenDialog(null);//sets position of dialog box to default(centre) of the screen
 				//alternatively, we can change parameter to "StudentFrame.this". This means thatdialog box will appear
 				//wherever the main frame is. 
 				
+
 			}
-			
+
 		});
 		file.add(load);
 		menu.add(file);
 		this.setJMenuBar(menu);
 		setJMenuBar(menu);
-		
-	    ArrayList<Student> students = new ArrayList<Student>();
-	    //Fetches all student details from the server and adds to the student ArrayList
-	    fetchStudentData(students);
+
+		ArrayList<Student> students = new ArrayList<Student>();
+		// Fetches all student details from the server and adds to the student
+		// ArrayList
+		fetchStudentData(students);
 
 		JList list = createJList(students);
 
-		
 		JTextField search = new JTextField(22);
 		search.addKeyListener(new KeyAdapter() {
-	        public void keyReleased(KeyEvent e) {
-	        	//clear the list
-	        	DefaultListModel listModel = (DefaultListModel) list.getModel();
-	            listModel.removeAllElements();
-	            //get whatever user types into text field
-	            String buffer = search.getText();
-	        	//store all matching students in serachStudent arraylist
-	        	for(Student i:students){
-	        		if (i.getName().toLowerCase().contains(buffer.toLowerCase()) || i.getStudentNumber().contains(buffer)){     
-	        			listModel.addElement(i);
-	        		}
-	        	}                                 
-	        }
-	     });
-		list.setFixedCellHeight(30);//cell formatting
-		list.setFixedCellWidth(250);//same thing
-		
-		//Sets top panel with search to borderLayout, so search JTextField 
-		//Stretches through the top dynamically
+			public void keyReleased(KeyEvent e) {
+				// clear the list
+				DefaultListModel listModel = (DefaultListModel) list.getModel();
+				listModel.removeAllElements();
+				// get whatever user types into text field
+				String buffer = search.getText();
+				// store all matching students in serachStudent arraylist
+				for (Student i : students) {
+					if (i.getName().toLowerCase()
+							.contains(buffer.toLowerCase())
+							|| i.getStudentNumber().contains(buffer)) {
+						listModel.addElement(i);
+					}
+				}
+			}
+		});
+		list.setFixedCellHeight(30);// cell formatting
+		list.setFixedCellWidth(260);// same thing
+
+		// Sets top panel with search to borderLayout, so search JTextField
+		// Stretches through the top dynamically
 		panel.setLayout(new BorderLayout());
-		panel.add(search,BorderLayout.WEST);
-	
-		//Adds the list as a ScrollPane so there is a scrollBar
-		add(new JScrollPane(list),BorderLayout.WEST);
+		panel.add(search, BorderLayout.WEST);
+
+		// Adds the list as a ScrollPane so there is a scrollBar
+		add(new JScrollPane(list), BorderLayout.WEST);
 		add(panel, BorderLayout.NORTH);
 
 		setVisible(true);
-		
+
 	}
-	
+
 	public JList createJList(ArrayList<Student> students){
 		
 	     DefaultListModel defListMod  = new DefaultListModel();//create a list of items that are editable. original list
@@ -126,66 +134,65 @@ public class StudentFrame extends JFrame{
 			 
 		 };
 		 list.addMouseListener(mouseListener);
-
-			
+		 list.setFont(new Font("Arial",Font.PLAIN,20));
 		 return list;
 	}
-	
-	public Student findStudent(String name, ArrayList<Student> studentArrayList){
+
+	public Student findStudent(String name, ArrayList<Student> studentArrayList) {
 		Student found = null;
-		for(int i = 0;i < studentArrayList.size();i++){
-			if(studentArrayList.get(i).toString().equals(name)){
+		for (int i = 0; i < studentArrayList.size(); i++) {
+			if (studentArrayList.get(i).toString().equals(name)) {
 				found = studentArrayList.get(i);
 			}
 		}
 		return found;
-		
-	}
-	
-	public void fetchStudentData(ArrayList<Student> students){
-		 // Create a Connector object and open the connection to the server
-        Connector server = new Connector();
-        boolean success = server.connect("TMH",
 
-"944ff2da7cd193c64ec9459a42f38786");
-        
-        if (success == false) {
-            System.out.println("Fatal error: could not open connection to server");
-            System.exit(1);
-        }
-        
-        DataTable data = server.getData();
-        
-        int rowCount = data.getRowCount();
-        ArrayList<String> studentDetails = new ArrayList<String>();
-        String tempWord = "";
-        //Loops through all data from server and puts into a studentDetail arrayList
-        for (int row = 0; row < rowCount; ++row) {
-            for (int col = 0; col < 4; ++col) {
-                if (col > 0) {
-                	tempWord += ",";
-                }
-                tempWord += data.getCell(row,col);
-            }
-            studentDetails.add(tempWord);
-            //Makes tempWord blank again
-            tempWord = "";
-        }
-        System.out.println(studentDetails.toString());
-        for(int i = 0;i < studentDetails.size();i++){
-        	String temp = studentDetails.get(i);
-        	//Splits the student details according to where the comma is
-        	String[] studentDetails1 = temp.split(",");
-        	int studentNumber = Integer.parseInt(studentDetails1[2]);
-        	
-            Student temp1 = new Student(studentDetails1[0],studentDetails1[1],studentNumber,studentDetails1[3]);
-        	students.add(temp1);
-        	
-        }
-		
 	}
 
-	
-	
+	public void fetchStudentData(ArrayList<Student> students) {
+		// Create a Connector object and open the connection to the server
+		Connector server = new Connector();
+		boolean success = server.connect("TMH",
+
+		"944ff2da7cd193c64ec9459a42f38786");
+
+		if (success == false) {
+			System.out
+					.println("Fatal error: could not open connection to server");
+			System.exit(1);
+		}
+
+		DataTable data = server.getData();
+
+		int rowCount = data.getRowCount();
+		ArrayList<String> studentDetails = new ArrayList<String>();
+		String tempWord = "";
+		// Loops through all data from server and puts into a studentDetail
+		// arrayList
+		for (int row = 0; row < rowCount; ++row) {
+			for (int col = 0; col < 4; ++col) {
+				if (col > 0) {
+					tempWord += ",";
+				}
+				tempWord += data.getCell(row, col);
+			}
+			studentDetails.add(tempWord);
+			// Makes tempWord blank again
+			tempWord = "";
+		}
+		System.out.println(studentDetails.toString());
+		for (int i = 0; i < studentDetails.size(); i++) {
+			String temp = studentDetails.get(i);
+			// Splits the student details according to where the comma is
+			String[] studentDetails1 = temp.split(",");
+			int studentNumber = Integer.parseInt(studentDetails1[2]);
+
+			Student temp1 = new Student(studentDetails1[0], studentDetails1[1],
+					studentNumber, studentDetails1[3]);
+			students.add(temp1);
+
+		}
+
+	}
 
 }
