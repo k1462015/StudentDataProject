7e7eff37b4dc140ca27acd69fb3312093f65359f
Hassan Minhas <haminhas@live.com>
2015-02-16 20:22:37 +0000

    created results class

---
+++ praCourseWork2/src/praCourseWork2/Result.java
@@ -0,0 +1,9 @@
+package praCourseWork2;
+
+public class Result {
+	protected String moduleCode;
+	protected int assessment;
+	protected String candKey;
+	protected int mark;
+	protected String grade;
+}
