c0358287336a2e1ac5e24c6167ff5fce3bfa57ad
Hassan Minhas <haminhas@live.com>
2015-02-16 20:24:32 +0000

    added constructor to results class

--- praCourseWork2/src/praCourseWork2/Result.java
+++ praCourseWork2/src/praCourseWork2/Result.java
@@ -6,4 +6,12 @@ public class Result {
 	protected String candKey;
 	protected int mark;
 	protected String grade;
+	
+	public Result(String moduleCode,int assessment,String candKey,int mark,String grade){
+		this.moduleCode = moduleCode;
+		this.assessment = assessment;
+		this.candKey = candKey;
+		this.mark = mark;
+		this.grade = grade;
+	}
 }
