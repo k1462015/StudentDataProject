7bf736ac53e5a8192b30bc9853047f03c0221940
Hassan Minhas <haminhas@live.com>
2015-02-26 11:37:52 +0000

    created assessment class and added constuctor

---
+++ praCourseWork2/src/praCourseWork2/Assessment.java
@@ -0,0 +1,11 @@
+package praCourseWork2;
+
+import java.util.ArrayList;
+
+public class Assessment {
+	private ArrayList<Student> students;
+	
+	public Assessment(){
+		students = new ArrayList<Student>();
+	}
+}
