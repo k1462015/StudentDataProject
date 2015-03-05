3aa08e7b2ceebc6bdcd0d22dc5b1d64c9eed2948
unknown <haminhas@live.com>
2015-03-05 22:56:43 +0000

    ive tried by best but cant get the select all functionality with a JList maybe one of u guys can give it a go

--- praCourseWork2/src/praCourseWork2/Email.java
+++ praCourseWork2/src/praCourseWork2/Email.java
@@ -2,16 +2,8 @@ package praCourseWork2;
 
 import java.awt.BorderLayout;
 import java.awt.Component;
-import java.awt.Dimension;
-import java.awt.Font;
-import java.awt.GridLayout;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
 import java.util.ArrayList;
 
-import javax.swing.BoxLayout;
-import javax.swing.DefaultListModel;
 import javax.swing.JButton;
 import javax.swing.JCheckBox;
 import javax.swing.JFrame;
@@ -19,8 +11,6 @@ import javax.swing.JList;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.ListCellRenderer;
-import javax.swing.ListModel;
-import javax.swing.UIManager;
 
 public class Email extends JFrame {
 	private JPanel main;
@@ -28,21 +18,22 @@ public class Email extends JFrame {
 	private JPanel west;
 	private JPanel buttons;
 	private JPanel listPanel;
+
 	public Email(ArrayList<Student> students){
-		
 		JButton selectAll = new JButton("select all");
 		JButton selectNone = new JButton("select none");
+		
 		main = new JPanel(new BorderLayout());
 		buttons = new JPanel();
 		listPanel = new JPanel();
 		west = new JPanel(new BorderLayout());
-		
+
 		setSize(550, 300);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
 		list = new JList(students.toArray());
 		list.setCellRenderer(new CheckboxListCellRenderer());
-		
+
 		buttons.add(selectNone);
 		buttons.add(selectAll);
 		
@@ -54,12 +45,13 @@ public class Email extends JFrame {
 		main.add(west, BorderLayout.WEST);
 		setVisible(true);
 		add(main);
-	}
+		}
 	
 	public class CheckboxListCellRenderer extends JCheckBox implements ListCellRenderer {
 
 	    public Component getListCellRendererComponent(JList list, Object value, int index, boolean isSelected, boolean cellHasFocus) {
-	        // need this in order to actually select the JCheckBox
+
+	    	// need this in order to actually select the JCheckBox
 	        setSelected(isSelected);
 	        setEnabled(list.isEnabled());
 	        // need this in order to display the student with checkbox
@@ -68,7 +60,7 @@ public class Email extends JFrame {
 	        setBackground(list.getBackground());
 	        setForeground(list.getForeground());
 	        return this;
-	    }
+	    }     
 	}
-	
 }
+
