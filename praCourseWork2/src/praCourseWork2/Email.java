7066b878e43bcccc8063f39c08630dcc52f8dded
unknown <haminhas@live.com>
2015-03-05 18:58:21 +0000

    set layout to border layout

--- praCourseWork2/src/praCourseWork2/Email.java
+++ praCourseWork2/src/praCourseWork2/Email.java
@@ -1,15 +1,18 @@
 package praCourseWork2;
 
+import java.awt.BorderLayout;
 import javax.swing.JFrame;
+import javax.swing.JList;
 import javax.swing.JPanel;
 
 public class Email extends JFrame {
 	private JPanel main;
-	
+	private JList list;
 	public Email(){
 		setVisible(true);
 		main = new JPanel();
-		main.setSize(300,300);
+		setSize(550, 300);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		main.setLayout(new BorderLayout());
 	}
 }
