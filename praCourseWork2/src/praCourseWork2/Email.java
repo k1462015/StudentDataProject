4f88eb4befa1e1da0bcfa7b91fc3e16acc9b3d3d
unknown <haminhas@live.com>
2015-03-05 20:16:42 +0000

    added the select all and select none buttons but havent added funtionlaiy

--- praCourseWork2/src/praCourseWork2/Email.java
+++ praCourseWork2/src/praCourseWork2/Email.java
@@ -2,13 +2,17 @@ package praCourseWork2;
 
 import java.awt.BorderLayout;
 import java.awt.Component;
+import java.awt.Dimension;
 import java.awt.Font;
+import java.awt.GridLayout;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.util.ArrayList;
 
+import javax.swing.BoxLayout;
 import javax.swing.DefaultListModel;
+import javax.swing.JButton;
 import javax.swing.JCheckBox;
 import javax.swing.JFrame;
 import javax.swing.JList;
@@ -21,15 +25,33 @@ import javax.swing.UIManager;
 public class Email extends JFrame {
 	private JPanel main;
 	private JList list;
-	
+	private JPanel west;
+	private JPanel buttons;
+	private JPanel listPanel;
 	public Email(ArrayList<Student> students){
-		main = new JPanel();
+		
+		JButton selectAll = new JButton("select all");
+		JButton selectNone = new JButton("select none");
+		main = new JPanel(new BorderLayout());
+		buttons = new JPanel();
+		listPanel = new JPanel();
+		west = new JPanel(new BorderLayout());
+		
 		setSize(550, 300);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		main.setLayout(new BorderLayout());
+		
 		list = new JList(students.toArray());
 		list.setCellRenderer(new CheckboxListCellRenderer());
-		main.add(new JScrollPane(list), BorderLayout.WEST);
+		
+		buttons.add(selectNone);
+		buttons.add(selectAll);
+		
+		listPanel.add(new JScrollPane(list));
+		
+		west.add(buttons,BorderLayout.NORTH);
+		west.add(listPanel, BorderLayout.CENTER);
+		
+		main.add(west, BorderLayout.WEST);
 		setVisible(true);
 		add(main);
 	}
@@ -37,7 +59,6 @@ public class Email extends JFrame {
 	public class CheckboxListCellRenderer extends JCheckBox implements ListCellRenderer {
 
 	    public Component getListCellRendererComponent(JList list, Object value, int index, boolean isSelected, boolean cellHasFocus) {
-	    	
 	        // need this in order to actually select the JCheckBox
 	        setSelected(isSelected);
 	        setEnabled(list.isEnabled());
