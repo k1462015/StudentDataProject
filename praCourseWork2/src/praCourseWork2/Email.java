a83d9e13f41c8947084095d88e5c5a1fc5a05c61
Hassan Minhas <haminhas@live.com>
2015-03-06 23:20:00 +0000

    added a table students to model

--- praCourseWork2/src/praCourseWork2/Email.java
+++ praCourseWork2/src/praCourseWork2/Email.java
@@ -12,18 +12,20 @@ import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.ListCellRenderer;
+import javax.swing.table.DefaultTableModel;
 
 public class Email extends JFrame {
 	private JPanel main;
-	private JTable list;
+	private JTable table;
 	private JPanel west;
 	private JPanel buttons;
 	private JPanel listPanel;
+	private ArrayList<Student> students;
 
-	public Email(ArrayList<Student> students){
+	public Email(ArrayList<Student> s){
 		JButton selectAll = new JButton("select all");
 		JButton selectNone = new JButton("select none");
-		
+		students =  new ArrayList<Student>(s);
 		main = new JPanel(new BorderLayout());
 		buttons = new JPanel();
 		listPanel = new JPanel();
@@ -43,6 +45,24 @@ public class Email extends JFrame {
 		setVisible(true);
 		add(main);
 		}
-	
+	public JScrollPane createTable(boolean b){
+		DefaultTableModel model = new DefaultTableModel();
+		
+		for (Student s : students) {
+			model.addRow(new Object[] {s.getName().replaceAll("\"", ""), b });
+		}
+		table = new JTable(model);
+
+		table.setPreferredScrollableViewportSize(new Dimension(300, 350));
+		table.setFillsViewportHeight(true);
+		table.setGridColor(Color.GRAY);
+		JScrollPane scrollPane = new JScrollPane(table);
+		
+		repaint();
+		revalidate();
+		
+		return scrollPane;
+	}
+
 }
 
