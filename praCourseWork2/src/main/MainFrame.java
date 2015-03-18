38426c0d26223285e03eb99d398e400c388f514c
Musta Rohman <mustarohman@gmail.com>
2015-03-18 23:51:49 +0000

    Amended the AverageListener (again...) so that the module code is displayed in the file name field when exporting

--- praCourseWork2/src/main/MainFrame.java
+++ praCourseWork2/src/main/MainFrame.java
@@ -486,7 +486,7 @@ public class MainFrame extends JFrame {
 					}
 				}
 
-				String modCode = (String) currentTable.getValueAt(0, 1);
+				String modCode = (String) currentTable.getValueAt(0, 2);
 				System.out.println("Making chart...");
 				ScatterPlot scatter = new ScatterPlot("Graph",
 						"Comparison of Average in Assessment", modCode, data);
