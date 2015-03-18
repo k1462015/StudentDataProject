a1084e372b2fb29696d7e4b75569b20fbc3f5edc
Musta Rohman <mustarohman@gmail.com>
2015-03-18 20:45:04 +0000

    Now reads and writes the settings file from the current directory, instead of the user's documents or desktop directory

--- praCourseWork2/src/email/Settings.java
+++ praCourseWork2/src/email/Settings.java
@@ -48,66 +48,27 @@ public class Settings {
 		// Create new settings file, save it in user's documents directory and
 		// write settings to
 		// that file
+		
+		String currentDirectory = new File("").getAbsolutePath();
+        currentDirectory += "\\settings.ini";
+        
+        File newFile = new File(currentDirectory);
+        newFile.getParentFile().mkdirs();
+        
+    	try {
+			newFile.createNewFile();
+			System.out.println("File created");
 
-		String OS = System.getProperty("os.name").toLowerCase();// Check's
-																// user's OS
+			PrintWriter writer = new PrintWriter(newFile);
+			writer.println(serverName + "," + portSpinner + "," + userName + "," + true);
+			writer.close();
+			System.out.println("File writed.");
 
-		if (OS.contains("windows")) {// If the OS is windows
-			String user = System.getProperty("user.name");
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
 
-			// Creates a new file path for the settings file within Documents
-			// directory
-			File newFile = new File("C:\\Users\\" + user + "\\Documents\\settings.ini");
-			newFile.getParentFile().mkdirs(); // Creates the necessary
-												// directories
-			try {
-				newFile.createNewFile();
-				System.out.println("File created");
-
-				PrintWriter writer = new PrintWriter(newFile);
-				writer.println(serverName + "," + portSpinner + "," + userName + "," + true);
-				writer.close();
-				System.out.println("File writed.");
-
-			} catch (IOException e1) {
-				// TODO Auto-generated catch block
-
-			}
-
-		} else if (OS.contains("mac")) {// If the OS is mac
-			String user = System.getProperty("user.name");
-			File newFile = new File("/Users/" + user + "/Desktop/settings.ini");
-			newFile.getParentFile().mkdirs();
-			try {
-				newFile.createNewFile();
-				System.out.println("File created");
-
-				PrintWriter writer = new PrintWriter(newFile);
-				writer.println(serverName + "," + portSpinner + "," + userName + "," + true);
-				writer.close();
-				System.out.println("File writed.");
-
-			} catch (IOException e1) {
-				// TODO Auto-generated catch block
-				e1.printStackTrace();
-			}
-		} else if (OS.contains("nix")) {// If the OS is mac/unix
-			File newFile = new File("~/Desktop/settings.ini");
-			newFile.getParentFile().mkdirs();
-			try {
-				newFile.createNewFile();
-				System.out.println("File created");
-
-				PrintWriter writer = new PrintWriter(newFile);
-				writer.println(serverName + "," + portSpinner + "," + userName + "," + true);
-				writer.close();
-				System.out.println("File writed.");
-
-			} catch (IOException e1) {
-				// TODO Auto-generated catch block
-
-			}
 		}
+
 
 	}
 
@@ -135,53 +96,24 @@ public class Settings {
 	}
 	
 	public File findSettingsFile() {
-		String OS = System.getProperty("os.name").toLowerCase();
-		File settings = null;
-		if (OS.contains("windows")) {
-			String user = System.getProperty("user.name");
-			String filePathStr = "C:\\Users\\" + user + "\\Documents";
-			System.out.println(filePathStr);
-			filePathStr += "\\settings.ini";
-			System.out.println(filePathStr);
+		
+        String currentDirectory = new File("").getAbsolutePath();
+        currentDirectory += "\\settings.ini";
+        
+        File settingsFile = null;
+        
+        File f = new File(currentDirectory);
+        
+        if (f.exists() && !f.isDirectory()){
+            System.out.println("Settings.ini exists");
+            settingsFile = f;
+        } else {
+            
+            System.out.println("Settings.ini doesn't exist yet");
+        }
 
-			File f = new File(filePathStr);
+        return settingsFile;
 
-			if (f.exists() && !f.isDirectory()) {
-				System.out.println("Settings.ini exists in windows");
-				settings = f;
-			}
-
-		} else if (OS.contains("mac")) {
-			String user = System.getProperty("user.name");
-			String filePathStr = "/Users/" + user + "/Desktop";
-			System.out.println(filePathStr);
-			filePathStr += "/settings.ini";
-			System.out.println(filePathStr);
-
-			File f = new File(filePathStr);
-
-			if (f.exists() && !f.isDirectory()) {
-				System.out.println("Settings.ini exists in macs");
-				settings = f;
-			}
-		}else if (OS.contains("nix")) {
-			String filePathStr = "~/Desktop";
-			System.out.println(filePathStr);
-			filePathStr += "/settings.ini";
-			System.out.println(filePathStr);
-
-			File f = new File(filePathStr);
-
-			if (f.exists() && !f.isDirectory()) {
-				System.out.println("Settings.ini exists");
-				settings = f;
-				
-			} else {
-				System.out.println("Settings.ini doesn't exist yet");
-				
-			}
-		}
-		return settings;
 	}
 
 }
